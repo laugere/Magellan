62155dd841139c09b55aea2571e314f8c39a486f
laugere <martin@justmagic.com>
2019-06-07 11:21:06 +0200

    Filter files create

--- Magellan.py
+++ Magellan.py
@@ -15,10 +15,12 @@ def GetObjectClass(CsvPath):
     return ObjectClass
 
 def OgrCommandLine(GeoJSONDestinationPath, DestinationFile, S57FilePath, ObjectClass):
-    subprocess.Popen('ogr2ogr -t_srs EPSG:4326 -f GeoJSON {0}/{1}.json {2} {3}'.format(GeoJSONDestinationPath, DestinationFile, S57FilePath, ObjectClass.lower()))
+    _cmd = os.system('ogr2ogr -progress -t_srs EPSG:4326 -f GeoJSON {0}/{1}.json {2} {3}'.format(GeoJSONDestinationPath, DestinationFile, S57FilePath, ObjectClass.lower()))
+    if _cmd == 1:
+        os.remove('{0}/{1}.json'.format(GeoJSONDestinationPath, DestinationFile))
 
 def ExtractToGeoJSON(S57FilePath, GeoJSONDestinationPath):
     for _object in GetObjectClass('s57objectclasses.csv'):
         OgrCommandLine(GeoJSONDestinationPath, str(_object), str(S57FilePath), str(_object))
 
-ExtractToGeoJSON('GeoJSON', 'ENC_ROOT/2W7D1870.000')
+ExtractToGeoJSON('ENC_ROOT/2W7D1870.000', 'GeoJSON')
