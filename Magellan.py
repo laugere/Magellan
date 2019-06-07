d568455c1e11136cb17dec602c2e2ebc34038861
laugere <martin@justmagic.com>
2019-06-07 12:27:30 +0200

    NOW MAGELLAN CAN EXPORT MULTIPLE CELLS IN FOLDER !

--- Magellan.py
+++ Magellan.py
@@ -1,6 +1,6 @@
 import csv
-import subprocess
 import os
+import glob
 
 
 def GetObjectClass(CsvPath):
@@ -14,13 +14,31 @@ def GetObjectClass(CsvPath):
     
     return ObjectClass
 
+def GetAllS57Repertory(S57FilesPath):
+    S57Path = []
+    for _file in glob.glob(('{0}{1}*.000').format(S57FilesPath, os.sep)):
+        S57Path.append('.'.join(_file.split('.')[:-1]))
+    
+    return S57Path
+
 def OgrCommandLine(GeoJSONDestinationPath, DestinationFile, S57FilePath, ObjectClass):
     _cmd = os.system('ogr2ogr -progress -t_srs EPSG:4326 -f GeoJSON {0}/{1}.json {2} {3}'.format(GeoJSONDestinationPath, DestinationFile, S57FilePath, ObjectClass.lower()))
     if _cmd == 1:
         os.remove('{0}/{1}.json'.format(GeoJSONDestinationPath, DestinationFile))
 
-def ExtractToGeoJSON(S57FilePath, GeoJSONDestinationPath):
-    for _object in GetObjectClass('s57objectclasses.csv'):
-        OgrCommandLine(GeoJSONDestinationPath, str(_object), str(S57FilePath), str(_object))
+def ExtractToGeoJSON(S57FilesPath, GeoJSONDestinationPath):
+    os.mkdir(GeoJSONDestinationPath + os.sep + S57FilesPath)
+    for _file in GetAllS57Repertory(S57FilesPath):
+        os.mkdir(GeoJSONDestinationPath+os.sep+_file)
+        for _object in GetObjectClass('s57objectclasses.csv'):
+            OgrCommandLine(GeoJSONDestinationPath+os.sep+_file, str(_object), str(_file+'.000'), str(_object))
 
-ExtractToGeoJSON('ENC_ROOT/2W7D1870.000', 'GeoJSON')
+
+
+
+
+########################################
+####### MAIN COMMAND FOR EXTRACT #######
+########################################
+ExtractToGeoJSON('ENC_ROOT', 'GeoJSON')
+
