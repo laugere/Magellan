7b66e917ba9886614170804329cd72c6b421069d
laugere <martin@justmagic.com>
2019-06-12 12:28:39 +0200

    UPDATE initMagellan

--- InitMagellan.py
+++ InitMagellan.py
@@ -1,28 +1,19 @@
 import os
 import sys
+import time
 
-def GDALArchIs64():
-    _cmd = os.system(r'setx PATH “%PATH%;C:\OSGeo4W64\bin”')
-    _cmd = os.system(r'setx GDAL_DATA “C:\OSGeo4W64\share\gdal”')
 
-def GDALArchIs32():
-    _cmd = os.system(r'setx PATH “%PATH%;C:\OSGeo4W\bin”')
-    _cmd = os.system(r'setx GDAL_DATA “C:\OSGeo4W\share\gdal”')
-
-def InitGDAL(GDALArch):
-    try:
-        if (int(GDALArch) == 64):
-            GDALArchIs64()
-            print('GDAL is initialized (64 architecture)')
-        elif (int(GDALArch) == 32):
-            GDALArchIs32()
-            print('GDAL is initialized (32 architecture)')
-        else:
-            print('ERROR : py InitMagellan.py < Architecture (64 or 32) >')
-    except:
-        print('ERROR : Not a valid type for architecture')
+def InitGDAL(GDALInstallPath, GDALDataPath):
+        _cmd = os.system('setx PATH "{0}"'.format(GDALInstallPath))
+        time.sleep( 1 )
+        _cmd = os.system('setx GDAL_DATA "{0}"'.format(GDALDataPath))
+        print('GDAL is initialized (32 architecture)')
 
 ########################################
 ####### MAIN COMMAND FOR INIT ##########
 ########################################
-InitGDAL(sys.argv[1])
+try:
+    InitGDAL(sys.argv[1], sys.argv[2])
+except:
+    print('ERROR : Not a valid path')
+    print('ERROR : py InitMagellan.py < GDAL installation folder path > < GDAL-data folder path >')
